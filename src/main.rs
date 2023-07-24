596471f0d42fee848f2b816041c71102ebe44205
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2023-07-25 00:24:05 +0200

    Add a timer struct

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
 mod draw;
 mod term;
+mod timer;
 
-use std::{
-    io::Error,
-    time::{Duration, Instant},
-};
+use std::{io::Error, time::Duration};
 
 use crossterm::event::{poll, read, Event, KeyCode};
 use draw::draw_block;
 use term::{close_terminal, init_terminal};
+use timer::Timer;
 
 fn main() -> Result<(), Error> {
     let mut term = init_terminal()?;
-    let start_time = Instant::now();
+    let mut timer = Timer::new();
 
     loop {
-        let elapsed = start_time.elapsed();
-        let minutes = elapsed.as_secs() / 60;
-        let seconds = elapsed.as_secs() - minutes * 60;
-        let tenths = (elapsed.as_millis() - (elapsed.as_secs() as u128) * 1000) / 100;
-        term.draw(|f| draw_block(f, &format!("{:02}{:02}{:01}", minutes, seconds, tenths)))?;
+        timer.tick();
+        term.draw(|f| draw_block(f, &format!("{}", timer)))?;
         if !poll(Duration::from_secs(0))? {
             continue;
         }
         match read()? {
             Event::Key(event) if event.code == KeyCode::Char('q') => break,
+            Event::Key(event) if event.code == KeyCode::Char('r') => timer.reset(),
             _ => (),
         };
     }
---
+++ src/timer.rs
@@ -0,0 +1,44 @@
+use std::{
+    fmt::{self, Display},
+    time::{Duration, Instant},
+};
+
+pub struct Timer {
+    start: Instant,
+    previous_tick: Instant,
+    current_tick: Instant,
+}
+
+impl Timer {
+    pub fn new() -> Timer {
+        Timer {
+            start: Instant::now(),
+            previous_tick: Instant::now(),
+            current_tick: Instant::now(),
+        }
+    }
+
+    pub fn tick(&mut self) {
+        self.previous_tick = self.current_tick;
+        self.current_tick = Instant::now();
+    }
+
+    pub fn reset(&mut self) {
+        self.start = Instant::now();
+        self.tick();
+    }
+
+    pub fn elapsed(&self) -> Duration {
+        self.current_tick.duration_since(self.start)
+    }
+}
+
+impl Display for Timer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let elapsed = self.elapsed();
+        let minutes = elapsed.as_secs() / 60;
+        let seconds = elapsed.as_secs() - minutes * 60;
+        let tenths = (elapsed.as_millis() - (elapsed.as_secs() as u128) * 1000) / 100;
+        write!(f, "{:02}{:02}{:01}", minutes, seconds, tenths)
+    }
+}
