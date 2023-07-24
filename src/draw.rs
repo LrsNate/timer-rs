a381378dc23dbeba54824f1a7b1dac86ccd841ea
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2023-07-24 21:25:50 +0200

    Draw thicker numbers

--- src/draw.rs
+++ src/draw.rs
@@ -1,67 +1,94 @@
-use std::io::Stdout;
+use std::{collections::HashMap, io::Stdout};
 
 use tui::{
     backend::CrosstermBackend,
     style::Color,
     symbols::Marker,
     widgets::{
-        canvas::{Canvas, Line},
+        canvas::{Canvas, Rectangle},
         Block, Borders,
     },
     Frame,
 };
 
-fn get_char_shape(x_offset: f64, y_offset: f64) -> Vec<Line> {
+fn get_char_shape(x_offset: f64, y_offset: f64, c: char) -> Vec<Rectangle> {
+    let char_codes = HashMap::from([
+        ('1', 18),
+        ('2', 93),
+        ('3', 91),
+        ('4', 58),
+        ('5', 107),
+        ('6', 111),
+        ('7', 82),
+        ('8', 127),
+        ('9', 123),
+    ]);
+    let code = char_codes[&c];
     let mut result = Vec::new();
-    result.push(Line {
-        x1: x_offset,
-        y1: y_offset,
-        x2: x_offset + 3.0,
-        y2: y_offset,
-        color: Color::White,
-    });
-    result.push(Line {
-        x1: x_offset,
-        y1: y_offset,
-        x2: x_offset,
-        y2: y_offset + 3.0,
-        color: Color::White,
-    });
-    result.push(Line {
-        x1: x_offset,
-        y1: y_offset + 3.0,
-        x2: x_offset + 3.0,
-        y2: y_offset + 3.0,
-        color: Color::White,
-    });
-    result.push(Line {
-        x1: x_offset + 3.0,
-        y1: y_offset,
-        x2: x_offset + 3.0,
-        y2: y_offset + 3.0,
-        color: Color::White,
-    });
-    result.push(Line {
-        x1: x_offset,
-        y1: y_offset + 3.0,
-        x2: x_offset,
-        y2: y_offset + 6.0,
-        color: Color::White,
-    });
-    result.push(Line {
-        x1: x_offset + 3.0,
-        y1: y_offset + 3.0,
-        x2: x_offset + 3.0,
-        y2: y_offset + 6.0,
-        color: Color::White,
-    });
-    result.push(Line {
-        x1: x_offset,
-        y1: y_offset + 6.0,
-        x2: x_offset + 3.0,
-        y2: y_offset + 6.0,
-        color: Color::White,
-    });
+    if code >> 6 & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset + 1.0,
+            y: y_offset + 10.0,
+            width: 4.0,
+            height: 1.0,
+            color: Color::White,
+        });
+    }
+    if code >> 5 & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset,
+            y: y_offset + 6.0,
+            width: 1.0,
+            height: 4.0,
+            color: Color::White,
+        });
+    }
+    if code >> 4 & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset + 5.0,
+            y: y_offset + 6.0,
+            width: 1.0,
+            height: 4.0,
+            color: Color::White,
+        });
+    }
+    if code >> 3 & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset + 1.0,
+            y: y_offset + 5.0,
+            width: 4.0,
+            height: 1.0,
+            color: Color::White,
+        });
+    }
+    if code >> 2 & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset,
+            y: y_offset + 1.0,
+            width: 1.0,
+            height: 4.0,
+            color: Color::White,
+        });
+    }
+    if code >> 1 & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset + 5.0,
+            y: y_offset + 1.0,
+            width: 1.0,
+            height: 4.0,
+            color: Color::White,
+        });
+    }
+    if code & 1 == 1 {
+        result.push(Rectangle {
+            x: x_offset + 1.0,
+            y: y_offset,
+            width: 4.0,
+            height: 1.0,
+            color: Color::White,
+        });
+    }
+
     result
 }
 
@@ -70,32 +97,39 @@ pub fn draw_block(f: &mut Frame<'_, CrosstermBackend<Stdout>>) {
     let canvas = Canvas::default()
         .block(Block::default().title("Canvas").borders(Borders::ALL))
         .marker(Marker::Braille)
-        .x_bounds([0.0, 20.0])
-        .y_bounds([0.0, 8.0])
+        .x_bounds([0.0, 31.0])
+        .y_bounds([0.0, 13.0])
         .paint(|ctx| {
             ctx.layer();
-            for line in get_char_shape(1.0, 1.0) {
+            for line in get_char_shape(1.0, 1.0, '2') {
                 ctx.draw(&line);
             }
-            for line in get_char_shape(5.0, 1.0) {
+            for line in get_char_shape(8.0, 1.0, '3') {
                 ctx.draw(&line);
             }
 
-            ctx.draw(&Line {
-                x1: 9.0,
-                y1: 3.0,
-                x2: 9.0,
-                y2: 3.0,
+            ctx.draw(&Rectangle {
+                x: 15.0,
+                y: 4.0,
+                width: 1.0,
+                height: 1.0,
                 color: Color::White,
             });
 
-            ctx.draw(&Line {
-                x1: 9.0,
-                y1: 5.0,
-                x2: 9.0,
-                y2: 5.0,
+            ctx.draw(&Rectangle {
+                x: 15.0,
+                y: 8.0,
+                width: 1.0,
+                height: 1.0,
                 color: Color::White,
             });
+
+            for line in get_char_shape(17.0, 1.0, '4') {
+                ctx.draw(&line);
+            }
+            for line in get_char_shape(24.0, 1.0, '5') {
+                ctx.draw(&line);
+            }
         });
     f.render_widget(canvas, size);
 }
