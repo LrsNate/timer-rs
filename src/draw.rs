8a14bc8157c44eea07f2f7c0db7355db82bfc86a
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2023-07-29 21:46:13 +0200

    Simplify chars separation

--- src/draw.rs
+++ src/draw.rs
@@ -2,15 +2,16 @@ use std::io::Stdout;
 
 use tui::{
     backend::CrosstermBackend,
-    symbols::Marker,
-    widgets::{canvas::Canvas, Block, Borders},
     Frame,
+    symbols::Marker,
+    widgets::{Block, Borders, canvas::Canvas},
 };
 
 use crate::shapes::{get_char_shape, get_separator_shape, get_small_char_shape};
 
 pub fn draw_block(f: &mut Frame<'_, CrosstermBackend<Stdout>>, s: &str) {
     let size = f.size();
+    let chars: Vec<char> = s.chars().collect();
     let canvas = Canvas::default()
         .block(Block::default().title("Canvas").borders(Borders::ALL))
         .marker(Marker::Braille)
@@ -19,11 +20,11 @@ pub fn draw_block(f: &mut Frame<'_, CrosstermBackend<Stdout>>, s: &str) {
         .paint(|ctx| {
             ctx.layer();
             let mut shapes = vec![];
-            shapes.append(&mut get_char_shape(1.0, 1.0, s.chars().nth(0).unwrap()));
-            shapes.append(&mut get_char_shape(9.0, 1.0, s.chars().nth(1).unwrap()));
+            shapes.append(&mut get_char_shape(1.0, 1.0, chars[0]));
+            shapes.append(&mut get_char_shape(9.0, 1.0, chars[1]));
             shapes.append(&mut get_separator_shape(16.0, 1.0));
-            shapes.append(&mut get_char_shape(18.0, 1.0, s.chars().nth(2).unwrap()));
-            shapes.append(&mut get_char_shape(26.0, 1.0, s.chars().nth(3).unwrap()));
+            shapes.append(&mut get_char_shape(18.0, 1.0, chars[2]));
+            shapes.append(&mut get_char_shape(26.0, 1.0, chars[3]));
             shapes.append(&mut get_small_char_shape(
                 34.0,
                 1.0,
